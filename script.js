95d448824671bbd7769e1888e76726e7dfaa78e9
Nathanlang14 <33582625+Nathanlang14@users.noreply.github.com>
2019-04-27 13:21:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var margin = {top: 20, right: 20, bottom: 35, left: 50},
-    width = 800 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    width = 700 - margin.left - margin.right,
+    height = 300 - margin.top - margin.bottom;
 
 var parseDate = d3.time.format("%Y-%m-%d").parse;
 
