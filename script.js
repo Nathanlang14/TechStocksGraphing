e7a37991615ce9a1d614e8819fb21a1d8114d017
Nathanlang14 <33582625+Nathanlang14@users.noreply.github.com>
2019-04-27 14:46:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ var line = d3.svg.line()
 var svg = d3.select("#chart").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-  .append("g")
+	.append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 svg.append("g")
@@ -67,94 +67,6 @@ svg.append("text")
 		x:0,
 		y:height + 30
 	});
-
-var mouseG = svg.append("g")
-  .attr("class", "mouse-over-effects");
-
-mouseG.append("path") // this is the black vertical line to follow mouse
-  .attr("class", "mouse-line")
-  .style("stroke", "black")
-  .style("stroke-width", "1px")
-  .style("opacity", "0");
-  
-var lines = document.getElementsByClassName('line');
-
-var mousePerLine = mouseG.selectAll('.mouse-per-line')
-  .data(companies)
-  .enter()
-  .append("g")
-  .attr("class", "mouse-per-line");
-
-mousePerLine.append("circle")
-  .attr("r", 7)
-  .style("stroke", function(d) {
-	return color(d.name);
-  })
-  .style("fill", "none")
-  .style("stroke-width", "1px")
-  .style("opacity", "0");
-
-mousePerLine.append("text")
-  .attr("transform", "translate(10,3)");
-
-mouseG.append('svg:rect') // append a rect to catch mouse movements on canvas
-  .attr('width', width) // can't catch mouse events on a g element
-  .attr('height', height)
-  .attr('fill', 'none')
-  .attr('pointer-events', 'all')
-  .on('mouseout', function() { // on mouse out hide line, circles and text
-	d3.select(".mouse-line")
-	  .style("opacity", "0");
-	d3.selectAll(".mouse-per-line circle")
-	  .style("opacity", "0");
-	d3.selectAll(".mouse-per-line text")
-	  .style("opacity", "0");
-  })
-  .on('mouseover', function() { // on mouse in show line, circles and text
-	d3.select(".mouse-line")
-	  .style("opacity", "1");
-	d3.selectAll(".mouse-per-line circle")
-	  .style("opacity", "1");
-	d3.selectAll(".mouse-per-line text")
-	  .style("opacity", "1");
-  })
-  .on('mousemove', function() { // mouse moving over canvas
-	var mouse = d3.mouse(this);
-	d3.select(".mouse-line")
-	  .attr("d", function() {
-		var d = "M" + mouse[0] + "," + height;
-		d += " " + mouse[0] + "," + 0;
-		return d;
-	  });
-
-	d3.selectAll(".mouse-per-line")
-	  .attr("transform", function(d, i) {
-		console.log(width/mouse[0])
-		var xDate = x.invert(mouse[0]),
-			bisect = d3.bisector(function(d) { return d.date; }).right;
-			idx = bisect(d.values, xDate);
-		
-		var beginning = 0,
-			end = lines[i].getTotalLength(),
-			target = null;
-
-		while (true){
-		  target = Math.floor((beginning + end) / 2);
-		  pos = lines[i].getPointAtLength(target);
-		  if ((target === end || target === beginning) && pos.x !== mouse[0]) {
-			  break;
-		  }
-		  if (pos.x > mouse[0])      end = target;
-		  else if (pos.x < mouse[0]) beginning = target;
-		  else break; //position found
-		}
-		
-		d3.select(this).select('text')
-		  .text(y.invert(pos.y).toFixed(2));
-		  
-		return "translate(" + mouse[0] + "," + pos.y +")";
-	  });
-  });
 
 
 
@@ -201,26 +113,98 @@ function draw(dataFile, axisFormat){
 	      })
 	    };
 	  });
+	  
+	  var mouseG = svg.append("g")
+		  .attr("class", "mouse-over-effects");
 
-	  /*
-	  	After we're done the companies array looks like this:
-		companies = [
-			{name: "AHC"
-			 values: [
-				{date: Mon Feb 23 2015 00:00:00 GMT-0600 (CST)
-				 price: 8.69 },
-				 etc...
-				]
-			},
-			{name: "NWS"
-			 values: [
-				{date: Mon Feb 23 2015 00:00:00 GMT-0600 (CST)
-				 price: 16.82 },
-				 etc...
-				]
-			},
-		]
-	  */
+		mouseG.append("path") // this is the black vertical line to follow mouse
+		  .attr("class", "mouse-line")
+		  .style("stroke", "black")
+		  .style("stroke-width", "1px")
+		  .style("opacity", "0");
+		  
+		var lines = document.getElementsByClassName('line');
+
+		var mousePerLine = mouseG.selectAll('.mouse-per-line')
+		  .data(companies)
+		  .enter()
+		  .append("g")
+		  .attr("class", "mouse-per-line");
+
+		mousePerLine.append("circle")
+		  .attr("r", 7)
+		  .style("stroke", function(d) {
+			return color(d.name);
+		  })
+		  .style("fill", "none")
+		  .style("stroke-width", "1px")
+		  .style("opacity", "0");
+
+		mousePerLine.append("text")
+		  .attr("transform", "translate(10,3)");
+
+		mouseG.append('svg:rect') // append a rect to catch mouse movements on canvas
+		  .attr('width', width) // can't catch mouse events on a g element
+		  .attr('height', height)
+		  .attr('fill', 'none')
+		  .attr('pointer-events', 'all')
+		  .on('mouseout', function() { // on mouse out hide line, circles and text
+			d3.select(".mouse-line")
+			  .style("opacity", "0");
+			d3.selectAll(".mouse-per-line circle")
+			  .style("opacity", "0");
+			d3.selectAll(".mouse-per-line text")
+			  .style("opacity", "0");
+		  })
+		  .on('mouseover', function() { // on mouse in show line, circles and text
+			d3.select(".mouse-line")
+			  .style("opacity", "1");
+			d3.selectAll(".mouse-per-line circle")
+			  .style("opacity", "1");
+			d3.selectAll(".mouse-per-line text")
+			  .style("opacity", "1");
+		  })
+		  .on('mousemove', function() { // mouse moving over canvas
+			var mouse = d3.mouse(this);
+			d3.select(".mouse-line")
+			  .attr("d", function() {
+				var d = "M" + mouse[0] + "," + height;
+				d += " " + mouse[0] + "," + 0;
+				return d;
+			  });
+
+			d3.selectAll(".mouse-per-line")
+			  .attr("transform", function(d, i) {
+				console.log(width/mouse[0])
+				var xDate = x.invert(mouse[0]),
+					bisect = d3.bisector(function(d) { return d.date; }).right;
+					idx = bisect(d.values, xDate);
+				
+				var beginning = 0,
+					end = lines[i].getTotalLength(),
+					target = null;
+
+				while (true){
+				  target = Math.floor((beginning + end) / 2);
+				  pos = lines[i].getPointAtLength(target);
+				  if ((target === end || target === beginning) && pos.x !== mouse[0]) {
+					  break;
+				  }
+				  if (pos.x > mouse[0])      end = target;
+				  else if (pos.x < mouse[0]) beginning = target;
+				  else break; //position found
+				}
+				
+				d3.select(this).select('text')
+				  .text(y.invert(pos.y).toFixed(2));
+				  
+				return "translate(" + mouse[0] + "," + pos.y +")";
+			  });
+		  });
+
+			  
+	  
+
 	  // You can print the companies data to the console and take a look.
 	  console.log(companies);
 
@@ -307,6 +291,7 @@ function draw(dataFile, axisFormat){
 	 			x: function(d, i){return width - 20 - (42 * i) ;},
 	 			y: 5,
 	 		});
+			
 
 	});
 
