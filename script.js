e6cf7d92d7c1d7e4e75e993fa999ce6d6ec25e2e
Nathanlang14 <33582625+Nathanlang14@users.noreply.github.com>
2019-05-02 03:46:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -162,13 +162,11 @@ function changer( arr ){
 			
 		  d3.select('.button-area').selectAll('.app-button')
 			.data(all_data)
-			//.enter().append('button')
+			.enter().append('button')
 			.append('button')
 			.attr('class', 'app-button')
 			.html(function(d) { return d.name; })
 			.on('click', function(d) {
-				console.log("b");
-				console.log(arr);
 			  data = d.data;
 			  updateChart();
 			});
