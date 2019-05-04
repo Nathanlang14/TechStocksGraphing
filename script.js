34592e59536a5fbff385f40873e7d23e16de44fb
Nathanlang14 <33582625+Nathanlang14@users.noreply.github.com>
2019-05-04 04:51:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -380,7 +380,7 @@ function changer( arr ){
 				});
 		  }
 		updateChart(data_1y, arr);
-			buttons = d3.selectAll('.button-area').remove();
+			buttons = d3.selectAll('.app-button').remove();
 			
 			console.log("after");
 			console.log(buttons);
